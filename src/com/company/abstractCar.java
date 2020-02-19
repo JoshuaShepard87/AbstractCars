86b94d0eb392578a60d5be50486ba6f6d9b11b54
pkrapf <paulkrapf@gmail.com>
2020-02-18 20:54:30 -0600

    Paul commit
    Nissan

---
+++ src/com/company/abstractCar.java
@@ -0,0 +1,11 @@
+package AbstractCar;
+
+abstract class abstractCar {
+    abstract public int topMPH();
+    abstract  public String brakes();
+    abstract public double engineSize();
+    abstract public int doorNumber();
+    abstract  public String make();
+    abstract public String model();
+    abstract public int year();
+}
