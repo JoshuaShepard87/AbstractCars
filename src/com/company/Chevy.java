38ae802504e96ac8736e3211ac0c9dd48f65b357
JoshuaShepard87 <jburden@my365.bellevue.edu>
2020-02-18 20:34:32 -0600

    First commit

--- src/com/company/Chevy.java
+++ src/com/company/Chevy.java
@@ -1,5 +1,52 @@
 package com.company;
 
-public class Chevy {
+public class Chevy extends Car{
+
+    int speed;
+    String brakes;
+    String engineSize;
+    int doors;
+    String Make;
+    String Model;
+
+    public Chevy(int speed, String brakes, String engineSize, int doors, String make, String model) {
+        this.speed = speed;
+        this.brakes = brakes;
+        this.engineSize = engineSize;
+        this.doors = doors;
+        Make = make;
+        Model = model;
+    }
+
+    @Override
+    public int MPH() {
+        return 145;
+    }
+
+    @Override
+    public String breaks() {
+        return "ABS";
+    }
+
+    @Override
+    public String engineSize() {
+        return "5.8L V8";
+    }
+
+    @Override
+    public int doorNumber() {
+        return 4;
+    }
+
+    @Override
+    public String Make() {
+        return "Chevrolet";
+    }
+
+    @Override
+    public String Model() {
+        return "Colorado";
+    }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Car chevy = new Chevy(80, "ABS", "5.7L V8",4, "Chevy", "Silverado");
+
+        Chevy chevy1 = new Chevy(43, "Nope", "rubber band", 2,"Chevy", "Clowncar");
+        chevy.printCar();
+        System.out.println("\n");
+        chevy1.printCar();
+
+
+
+
     }
+
 }
